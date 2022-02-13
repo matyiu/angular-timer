5253d881a1ba52d88a6dce19a1fc1c01f57cabe3
Jefferson Valle <valleespinozajefferson@gmail.com>
2022-02-13 12:31:30 +0100

    Replaced input view for print timer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -56,6 +56,10 @@ export class AppComponent {
     this.focus = !this.focus;
   }
 
+  focusInput(input: HTMLInputElement) {
+    input.focus();
+  }
+
   private setCurrent() {
     const { hours, minutes, seconds } = this.parseTimer(String(this.timer));
 
